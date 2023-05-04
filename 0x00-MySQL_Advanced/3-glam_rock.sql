df77f5b71aef4e758ebc485c053a93bdad8170d1
Peter Chibunna <peterchibunna@gmail.com>
2023-05-04 19:37:04 +0100

    Task #3

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -9,5 +9,5 @@
 
 SELECT `band_name`, (IFNULL(`split`, '2023') - `formed`) AS lifespan
 FROM `metal_bands`
-WHERE FIND_IN_SET('Glam rock', IFNULL(`style`, '')) >= 1
+WHERE FIND_IN_SET('Glam rock', IFNULL(`style`, '')) > 0
 ORDER BY `lifespan` DESC;
