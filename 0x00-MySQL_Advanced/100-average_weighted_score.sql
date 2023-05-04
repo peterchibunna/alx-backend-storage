17359f36a0e152d89aa9d08e4cf7c5eb298bc203
Peter Chibunna <peterchibunna@gmail.com>
2023-05-04 21:34:57 +0100

    12. Average weighted score

---
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -0,0 +1,17 @@
+-- creates a stored procedure ComputeAverageWeightedScoreForUser that
+-- computes and store the average weighted score for a student.
+DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUser;
+DELIMITER //
+CREATE PROCEDURE ComputeAverageWeightedScoreForUser (user_id INT)
+BEGIN
+    DECLARE weighted_average FLOAT DEFAULT 0;
+
+    SELECT AVG(`corrections`.`score` * `projects`.`weight`)
+        INTO weighted_average FROM `corrections` LEFT JOIN `projects`
+            ON `corrections`.`project_id` = `projects`.`id` WHERE `corrections`.`user_id` = user_id;
+
+    UPDATE `users`
+            SET `users`.`average_score` = weighted_average
+            WHERE `users`.`id` = user_id;
+END //
+DELIMITER ;
