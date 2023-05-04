c2414ef018b9718203b55a1ca90f2a37a83183c6
Peter Chibunna <peterchibunna@gmail.com>
2023-05-04 21:39:16 +0100

    12. Average weighted score

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -7,11 +7,12 @@ BEGIN
     DECLARE weighted_average FLOAT DEFAULT 0;
 
     SELECT AVG(`corrections`.`score` * `projects`.`weight`)
-        INTO weighted_average FROM `corrections` LEFT JOIN `projects`
+        INTO weighted_average
+        FROM `corrections` LEFT JOIN `projects`
             ON `corrections`.`project_id` = `projects`.`id` WHERE `corrections`.`user_id` = user_id;
 
     UPDATE `users`
-            SET `users`.`average_score` = weighted_average
+            SET `users`.`average_score` = IFNULL(weighted_average, 0)
             WHERE `users`.`id` = user_id;
 END //
 DELIMITER ;
