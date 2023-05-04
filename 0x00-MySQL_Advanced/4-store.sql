92542799c694d90be0eda16ec2167193977e43f1
Peter Chibunna <peterchibunna@gmail.com>
2023-05-04 20:09:07 +0100

    Task 5

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -2,7 +2,7 @@
 -- Quantity in the table items can be negative.
 -- Context: Updating multiple tables for one action from your application can generate issue: network disconnection, crash, etc… to keep your data in a good shape, let MySQL do it for you!
 DROP TRIGGER IF EXISTS update_stock;
-DELIMITER $$
+DELIMITER //
 CREATE TRIGGER update_stock
 AFTER INSERT ON `orders`
 FOR EACH ROW
@@ -10,5 +10,5 @@ BEGIN
     UPDATE `items`
         SET `quantity` = `quantity` - NEW.`number`
         WHERE `name` = NEW.`item_name`;
-END $$
+END //
 DELIMITER ;
