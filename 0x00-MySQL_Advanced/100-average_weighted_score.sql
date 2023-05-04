c12a8adf48665493f5e5e32beedfbca1c2440a59
Peter Chibunna <peterchibunna@gmail.com>
2023-05-04 21:55:34 +0100

    12. Average weighted score. 6

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -4,25 +4,30 @@ DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUser;
 DELIMITER //
 CREATE PROCEDURE ComputeAverageWeightedScoreForUser (user_id INT)
 BEGIN
-    DECLARE weighted_average FLOAT DEFAULT 0;
-    DECLARE project_weight FLOAT DEFAULT 0;
+    DECLARE sum_weighted_score INT DEFAULT 0;
+    DECLARE project_weight INT DEFAULT 0;
+
+    SELECT SUM(corrections.score * projects.weight)
+    INTO sum_weighted_score
+    FROM corrections
+             LEFT JOIN projects
+                        ON corrections.project_id = projects.id
+    WHERE corrections.user_id = user_id;
 
     SELECT SUM(projects.weight)
-        INTO project_weight
-        FROM corrections LEFT JOIN projects ON corrections.project_id = projects.id
-        WHERE corrections.user_id = user_id;
-    
-    SELECT AVG(`corrections`.`score` * `projects`.`weight`)
-        INTO weighted_average
-        FROM `corrections` INNER JOIN `projects`
-            ON `corrections`.`project_id` = `projects`.`id` WHERE `corrections`.`user_id` = user_id;
-    IF project_weight <> 0 THEN
-        UPDATE `users`
-            SET `users`.`average_score` = IFNULL(weighted_average, 0)
-            WHERE `users`.`id` = user_id;
-    ELSE
+    INTO project_weight
+    FROM corrections
+             LEFT JOIN projects
+                        ON corrections.project_id = projects.id
+    WHERE corrections.user_id = user_id;
+
+    IF project_weight = 0 THEN
         UPDATE users
         SET users.average_score = 0
+        WHERE users.id = user_id;
+    ELSE
+        UPDATE users
+        SET users.average_score = sum_weighted_score / project_weight
         WHERE users.id = user_id;
     END IF;
 END //
