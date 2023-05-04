4a7cb5aa3835fbd058305b96e327eff5f27ddccd
Peter Chibunna <peterchibunna@gmail.com>
2023-05-04 20:23:53 +0100

    Task #7

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -6,9 +6,10 @@ CREATE PROCEDURE ComputeAverageScoreForUser (user_id INT)
 BEGIN
     DECLARE total_score INT DEFAULT 0;
     DECLARE projects_count INT DEFAULT 0;
+    DECLARE average_score FLOAT DEFAULT 0;
 
-    SELECT SUM(score)
-        INTO total_score
+    SELECT AVG(score)
+        INTO average_score
         FROM `corrections`
         WHERE `corrections`.user_id = user_id;
     SELECT COUNT(*)
@@ -17,7 +18,7 @@ BEGIN
         WHERE `corrections`.user_id = user_id;
 
     UPDATE users
-        SET users.average_score = IF(projects_count = 0, 0, total_score / projects_count)
+        SET users.average_score = average_score
         WHERE users.id = user_id;
 END //
 DELIMITER ;
