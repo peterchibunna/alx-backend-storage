5f485b20c0fa7928e3c4cece8d27539d312163fb
Peter Chibunna <peterchibunna@gmail.com>
2023-05-11 09:31:52 +0100

    Task 0

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+0x02. Redis basic
+- Learn how to use redis for basic operations
+- Learn how to use redis as a simple cache
+"""
+import redis
+import typing
+import uuid
+
+
+class Cache:
+    def __init__(self) -> None:
+        """initialise the redis cache
+        """
+        self._redis = redis.Redis()
+        self._redis.flushdb(asynchronous=True)
+
+    def store(self, data: typing.Union[str, bytes, int, float]) -> str:
+        """0. Writing strings to Redis
+        """
+        key = uuid.uuid4().__str__()
+        self._redis.set(key, data)
+        return key
